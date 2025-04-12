e66214ef1cec152453e2c81297e8bb97d7794798
高橋亜弓 <takahashiayumi@MacBook-Air.local>
2025-04-13 00:18:53 +0900

    デプロイの設定

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.bundle.min.js"
+pin "bootstrap" # @5.3.5
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
