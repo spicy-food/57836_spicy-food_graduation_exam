796d8f09f167f96118f16c4f458cf968af49aac3
高橋亜弓 <takahashiayumi@MacBook-Air.local>
2025-06-24 23:16:45 +0900

    usersテーブルとcoffee_recordsテーブルのマイグレーションを作成

---
+++ db/migrate/20250624141524_create_users.rb
@@ -0,0 +1,25 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :encrypted_password
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
+      t.integer :sign_in_count
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
+      t.string :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string :unconfirmed_email
+      t.integer :failed_attempts
+      t.string :unlock_token
+      t.datetime :locked_at
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_15_113340) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_24_141524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "coffee_records", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "origin"
-    t.string "bean_type"
-    t.string "process_method", null: false
-    t.string "roast_level", null: false
-    t.string "acidity_level", null: false
-    t.string "body_level", null: false
-    t.text "notes"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "product_name"
-    t.string "name"
-    t.string "variety"
-    t.string "process"
-    t.string "roast"
-    t.string "acidity"
-    t.string "body"
-    t.index ["user_id"], name: "index_coffee_records_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email"
+    t.string "encrypted_password"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count"
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts"
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "coffee_records", "users"
 end
