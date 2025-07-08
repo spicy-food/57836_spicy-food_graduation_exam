481e1078b8f698f36617d21b7953b79af4b3e249
高橋亜弓 <takahashiayumi@MacBook-Air.local>
2025-07-08 23:10:44 +0900

    javascriptの修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,11 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application"
+
+// すべてのコントローラを自動で読み込む
+const context = require.context(".", true, /\.js$/)
+context.keys().forEach(filename => {
+  if (filename === "./application.js" || filename === "./index.js") return
+  application.register(
+    filename.replace("./", "").replace("_controller.js", ""),
+    context(filename).default
+  )
+})
