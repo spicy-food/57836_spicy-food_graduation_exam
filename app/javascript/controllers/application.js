94646f40c5ab73314c34aa182f879ad83b1d00fa
高橋亜弓 <takahashiayumi@MacBook-Air.local>
2025-07-15 19:42:50 +0900

    オートコンプリート機能の実装

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,6 @@ const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus   = fapplication
 
 export { application }
