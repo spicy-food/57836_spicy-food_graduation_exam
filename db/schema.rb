e761c0a320b75bfae6c2dffe904c70af7c35a507
高橋亜弓 <takahashiayumi@MacBook-Air.local>
2025-07-19 21:55:53 +0900

    問い合わせフォームの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_15_151904) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_19_072550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_15_151904) do
     t.text "notes"
     t.string "image"
     t.index ["user_id"], name: "index_coffee_records_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "subject"
+    t.text "message"
+    t.string "category"
+    t.boolean "read"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
